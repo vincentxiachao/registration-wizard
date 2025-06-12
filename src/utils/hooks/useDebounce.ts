292df7d356175b67b9ba96dd095bba8964b2a05a
Chao <vincentxia0625@163.com>
2025-06-12 12:09:02 +0800

    fix type of timeout

--- src/utils/hooks/useDebounce.ts
+++ src/utils/hooks/useDebounce.ts
@@ -1,7 +1,7 @@
 import { useRef } from 'react';
 
 export const useDebounce = <T>(cb: (args?: T) => void, delay: number = 500) => {
-  const timerRef = useRef<number | null>(null);
+  const timerRef = useRef<NodeJS.Timeout | null>(null);
   const debounceCb = <U extends T>(...args: U[]) => {
     if (timerRef.current) {
       clearTimeout(timerRef.current);
